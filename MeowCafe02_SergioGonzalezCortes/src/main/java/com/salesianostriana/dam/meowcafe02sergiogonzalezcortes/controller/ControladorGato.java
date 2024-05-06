7700ecb025c61f6e185d539784d0bee25a8d3a8c
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-05-06 10:41:32 +0200

    Editar gatos terminado

--- MeowCafe02_SergioGonzalezCortes/src/main/java/com/salesianostriana/dam/meowcafe02sergiogonzalezcortes/controller/ControladorGato.java
+++ MeowCafe02_SergioGonzalezCortes/src/main/java/com/salesianostriana/dam/meowcafe02sergiogonzalezcortes/controller/ControladorGato.java
@@ -3,10 +3,7 @@ package com.salesianostriana.dam.meowcafe02sergiogonzalezcortes.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.salesianostriana.dam.meowcafe02sergiogonzalezcortes.model.Gato;
 import com.salesianostriana.dam.meowcafe02sergiogonzalezcortes.service.ServicioGato;
@@ -31,13 +28,13 @@ public class ControladorGato {
 		
 		Gato gato = new Gato();
 		
-		model.addAttribute("gatoNuevo", gato);
+		model.addAttribute("gato", gato);
 		
 		return "formularioGatitos";
 	}
 	
 	@PostMapping("/formularioGatos/agregar")
-	public String agregarGatinio (@ModelAttribute("gatoNuevo") Gato gato) {
+	public String agregarGatinio (@ModelAttribute("gato") Gato gato) {
 		
 		servicioGato.save(gato);
 		
@@ -46,7 +43,7 @@ public class ControladorGato {
 	}
 	
 	@GetMapping("/formularioGatos/{id}")
-	public String formularioEditar(@RequestParam("id") long id, Model model) {
+	public String formularioEditar(@PathVariable("id") long id, Model model) {
 		
 		model.addAttribute("gato", servicioGato.findById(id).get());
 		
