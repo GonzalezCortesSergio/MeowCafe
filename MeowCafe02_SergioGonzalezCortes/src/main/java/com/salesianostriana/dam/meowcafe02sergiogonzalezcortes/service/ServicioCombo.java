95b935f86ebdf7f45b935f37638d244df9c5f3e0
Sergio González Cortés <gonzalez.coser23@triana.salesianos.edu>
2024-05-13 22:31:09 +0200

    Agregar combos terminado

--- MeowCafe02_SergioGonzalezCortes/src/main/java/com/salesianostriana/dam/meowcafe02sergiogonzalezcortes/service/ServicioCombo.java
+++ MeowCafe02_SergioGonzalezCortes/src/main/java/com/salesianostriana/dam/meowcafe02sergiogonzalezcortes/service/ServicioCombo.java
@@ -2,6 +2,7 @@ package com.salesianostriana.dam.meowcafe02sergiogonzalezcortes.service;
 
 import com.salesianostriana.dam.meowcafe02sergiogonzalezcortes.model.Producto;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.meowcafe02sergiogonzalezcortes.model.Combo;
@@ -11,4 +12,23 @@ import com.salesianostriana.dam.meowcafe02sergiogonzalezcortes.service.base.Serv
 @Service
 public class ServicioCombo extends ServicioBaseImpl<Combo, Long, RepositorioCombo>{
 
+    @Autowired
+    private ServicioProducto servicioProducto;
+
+
+    public void addProducto(Combo combo, Producto producto) {
+
+        combo.getProducto().add(producto);
+
+        producto.getCombo().add(combo);
+
+        servicioProducto.edit(producto);
+    }
+
+    public void obtenerPrecioCombo(Combo combo) {
+
+        combo.setPrecioCombo(combo.getProducto().stream()
+                .mapToDouble(Producto::getPrecio)
+                .sum());
+    }
 }
