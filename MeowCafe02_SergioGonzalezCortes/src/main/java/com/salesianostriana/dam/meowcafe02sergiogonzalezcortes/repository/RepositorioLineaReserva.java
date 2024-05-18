06befa8be409f414a8137ac389829e3dc078b3b8
Sergio González Cortés <gonzalez.coser23@triana.salesianos.edu>
2024-05-18 20:17:46 +0200

    Página antes de reservar hecha

---
+++ MeowCafe02_SergioGonzalezCortes/src/main/java/com/salesianostriana/dam/meowcafe02sergiogonzalezcortes/repository/RepositorioLineaReserva.java
@@ -0,0 +1,7 @@
+package com.salesianostriana.dam.meowcafe02sergiogonzalezcortes.repository;
+
+import com.salesianostriana.dam.meowcafe02sergiogonzalezcortes.model.LineaReserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioLineaReserva extends JpaRepository<LineaReserva, Long> {
+}
